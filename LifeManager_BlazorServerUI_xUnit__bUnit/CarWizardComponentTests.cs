4b9674959cb82b2df2a87e2ffab02263f5b52265
rpd4g5 <ryanpd423@gmail.com>
2021-01-19 23:55:09 -0600

    add bUnit + xUnit Test Project for Testing the Blazor UI

---
+++ LifeManager_BlazorServerUI_xUnit__bUnit/CarWizardComponentTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Bunit;
+using LifeManager_BlazorServerUI.Components;
+using System.Linq;
+using Xunit;
+using LifeManager_BlazorServerUI.ViewModels;
+using Moq;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LifeManager_BlazorServerUI_xUnit__bUnit
+{
+    public class CarWizardComponentTests : IClassFixture<CarWizardComponentFixture>
+    {
+        private readonly CarWizardComponentFixture _carWizardComponentFixture;
+
+        public CarWizardComponentTests(CarWizardComponentFixture carWizardComponentFixture)
+        {
+            _carWizardComponentFixture = carWizardComponentFixture;
+        }
+
+        [Fact]
+        public void it_should_do_something()
+        {
+            // Act
+            var page = _carWizardComponentFixture.bUnitTestContext
+                .RenderComponent<CarWizardComponent>();
+            // var result = page.Find("div").ClassName(Xunit =>);
+
+            // Assert
+
+        }
+    }
+
+    public class CarWizardComponentFixture : TestContext
+    {
+
+        // Private Property Mocked Injected Dependencies
+        private readonly Mock<ICarWizardViewModel> _mockCarWizardViewModel;
+
+        // Public Property for bUnit Test Context
+        public readonly TestContext bUnitTestContext;
+
+        public CarWizardComponentFixture()
+        {
+            _mockCarWizardViewModel = new Mock<ICarWizardViewModel>();
+            bUnitTestContext = new TestContext();
+            bUnitTestContext.Services.AddSingleton(_mockCarWizardViewModel.Object);
+        }
+    }
+}
---
+++ LifeManager_BlazorServerUI_xUnit__bUnit/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LifeManager_BlazorServerUI_xUnit__bUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
